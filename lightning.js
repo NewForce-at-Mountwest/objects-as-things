9c2e958a8bf4a23c587485f0c4df5b0a72087df6
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-01-23 19:43:19 -0500

    boilerplate demo code

---
+++ lightning.js
@@ -0,0 +1,25 @@
+// ----------------- ONE -------------------------//
+
+// 1. Build three objects. Each one should represent an employee at the coffee shop.
+// 2. Each employee object should have the following properties:
+// - a name
+// - a startDate
+// - a jobTitle
+// - a payRate
+// - a favoriteDrink
+// - an array of drinks they know how to make
+
+// 2. Build another object called coffeeShopStaff
+// 3. This object should have two properties:
+// - a property of fullTime that holds an array of full time staff members
+// - a property of partTime that holds an array of part time staff members
+// - Put two of your employee objects from steps 1 & 2 into the part time array. Put one of them in the full time array.
+
+
+// --------------------- TWO ------------------------------//
+
+// 1. Remember your employee objects from up there? Give one of them a method called makeCoffeDrink
+// 2. This method should accept a parameter of drink and return a string of "Here's your [drink]!" 
+// 3. Call the method and pass in any drink (string) you want
+// 4. Call the method (be sure to catch the returned string in a variable!)
+// Bous: If you have time, refactor your method so that it checks to see if the employee knows how to make the drink (i.e. if it's in the array of drinks they know how to make). If they do, you should return the string from step 2. If not, you should return a string that says "No can do!"
---
+++ script.js
@@ -0,0 +1,22 @@
+// Back to the coffee shop example!
+
+// Define a couple of objects that represent coffee drinks
+
+
+// Define a bigger object that represents the coffee shop as a whole
+// It should have a property called "menu" that holds an array of the coffee drink objects we just made
+
+
+// How can we change the data in an object? Say we want to change the coffee shop's open hours? Or add something to the menu?
+
+// The current values in an object are called the object's "state". The state can change!
+
+// ---------- LIGHTNING EXERCISE ONE ------------------------- //
+
+// Define an object that represents a customer
+
+// What are the behaviors/ actions a customer cna take in the coffee shop? Buy a coffee? Complain to the manager?
+
+// Methods can also accept parameters!
+
+// briefly discuss this
